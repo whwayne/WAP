c58d12c75330e67ca4ad9b807b160395cd981843
whwayne <whwayne@gmail.com>
2019-07-09 00:53:22 -0500

    first

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello, World!");
+        /*PrintWriter out = resp.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<form method='post'>");
+        out.print("<p>Please click the button</p>");
+        out.print("<input type='submit' value='Click me'/>");
+        out.print("</form>");
+        out.print("</body></html>");*/
+    }
+/*
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<p>Postback received</p>");
+        out.print("</body></html>");
+    }
+
+ */
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: whwayne
+  Date: 2019-07-08
+  Time: 20:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href='simple'>Start the simplest servlet app</a>
+  $END$
+  </body>
+</html>
